a42ee1aed6f999838546ec105898ba0643cff382
msbense <MSB1x_000@MB-SP.home>
2014-10-12 16:55:07 -0400

    Added functionality for saving and loading reports to the local machine (in the app directory).

--- src/lsrobotics/safety/docs/MongoDataSource.java
+++ src/lsrobotics/safety/docs/MongoDataSource.java
@@ -7,9 +7,14 @@
 package lsrobotics.safety.docs;
 
 import com.thoughtworks.xstream.XStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,29 +26,45 @@ class MongoDataSource implements IDataSource {  //MongoDB is a database
                                                 //application: mongodb.org
                                                 
                                                 //Perfect for storing safety docs
+    public MongoDataSource(){
+        
+    }
     public void Upload(Report r) {
         
     }
 
     public void SaveLocal(Report r) {
         XStream serializer = new XStream(); //XML Serializer: saves objects to local
+        serializer.alias("report", Report.class);
         String data = serializer.toXML(r);  //machine
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(r.name)); //writes to the app directory for now
+            BufferedWriter writer = new BufferedWriter(new FileWriter(r.name + ".xml")); //writes to the app directory for now
             writer.write(data);
             writer.close();
         } catch (IOException ex) {
             System.out.println("Could not open a file to write");
         }
     }
-
-
-    public Report Download() {
+    
+    public Report Download(String fileName) {
         return new Report(); //todo
     }
     
-    public Report LoadLocal() {
-        return new Report(); //todo
+    public Report LoadLocal(String fileName) {
+        fileName += ".xml";
+        XStream serializer = new XStream();
+        serializer.alias("report", Report.class);
+        Report r = null;
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            String xml = scanner.useDelimiter("\\Z").next(); //read the entire file
+            r = (Report) serializer.fromXML(xml);
+            return r;
+        } catch (FileNotFoundException ex) {
+            System.out.println("Could not load the file requested");
+        }
+        if (r != null) return r;
+        else return new Report();
     }
     
 }
