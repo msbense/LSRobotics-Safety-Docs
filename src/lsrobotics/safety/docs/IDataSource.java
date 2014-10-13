5ddfd3ae6866e9b015a4e48c9c06b383a3387641
msbense <MSB1x_000@MB-SP.home>
2014-10-13 11:20:18 -0400

    Added comments to IDataSource and Report

--- src/lsrobotics/safety/docs/IDataSource.java
+++ src/lsrobotics/safety/docs/IDataSource.java
@@ -4,16 +4,19 @@ package lsrobotics.safety.docs;
 /**
  *
  * @author msbense
- */                                 /*We do not have a storage method set in stone
-                                      but generally we will be working with these
-                                      four operations; the actual object can change*/
+ */                                 /*We do not have a storage method set in stone,
+                                      so I created an interface to manage data 
+                                      generally. 
+                                      
+                                      Feel free to change the interface so I know
+                                      what would be useful to the front-end*/
 public interface IDataSource {
     public void Upload(Report r); //upload a report to the internet/a database
     
-    public Report Download();
+    public Report Download(String fileName);
     
     public void SaveLocal(Report r); //save it locally; we probably won't have a
                                      //connection at competitions
-    public Report LoadLocal();
+    public Report LoadLocal(String fileName);
 }
 
