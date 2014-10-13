bff4e561187627e226dc95d8f75993995706d6dd
msbense <MSB1x_000@MB-SP.home>
2014-10-13 09:44:18 -0400

    Added comments to IDataSource and Report

--- src/lsrobotics/safety/docs/MongoDataSource.java
+++ src/lsrobotics/safety/docs/MongoDataSource.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package lsrobotics.safety.docs;
 
 import com.thoughtworks.xstream.XStream;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -24,7 +14,6 @@ import java.util.logging.Logger;
  */
 class MongoDataSource implements IDataSource {  //MongoDB is a database 
                                                 //application: mongodb.org
-                                                
                                                 //Perfect for storing safety docs
     public MongoDataSource(){
         
